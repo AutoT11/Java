3dbf16874a56f6a4f1caab80dbbbc8bb15565a64
gpbu62589 <ivannikov256@mail.ru>
2026-09-18 12:02:03 +0300

    новый тест по бд от гемини

--- src/main/java/API/untils/TestNumberGenerator.java
+++ src/main/java/API/untils/TestNumberGenerator.java
@@ -5,9 +5,22 @@ import  java.util.concurrent.ThreadLocalRandom;
 //Рандомный "tittle"
 public class TestNumberGenerator {
 
+    // Метод для названий (например, для кредитных карт).
     public static String uniqueTitle() {
         int randomNumber = ThreadLocalRandom.current().nextInt(100000, 1000000);
         return "Кредитка особенная_АТ_" + randomNumber;
 
     }
+
+    public static String uniqueUserName() {
+        int randomNumber = ThreadLocalRandom.current().nextInt(100000, 1000000);
+        return "Test user_АТ_" + randomNumber;
+    }
+
+    public static String uniqueUserEmail() {
+        int randomNumber = ThreadLocalRandom.current().nextInt(100000, 1000000);
+        return "testuser_АТ_" + randomNumber + "@gmail.com";
+    }
+
+
 }
