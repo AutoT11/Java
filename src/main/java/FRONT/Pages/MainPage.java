cd69c5269786c5d78f7a2094208cbe9297326232
gpbu62589 <ivannikov256@mail.ru>
2026-09-19 13:48:47 +0300

    самый актуал с мака

---
+++ src/main/java/FRONT/Pages/MainPage.java
@@ -0,0 +1,18 @@
+package FRONT.Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+
+public class MainPage extends BasePage {
+
+    private final By cookieButton = By.xpath("//button[text()='Хорошо']");
+    private final By menuButton = By.xpath("//div[contains(@class, 'MenuIcon-header')]");
+    private final By menuCredits = By.xpath("//div[contains(@class, 'MenuIcon-header')]");
+
+
+
+
+
+}
